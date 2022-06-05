de0b367b96fb0bc2b3eeaec7bc57fbad77c2c4eb
Your Name <you@example.com>
2022-06-05 17:53:06 -0400

    if/else statements added

--- script.js
+++ script.js
@@ -15,27 +15,45 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   // var password = generatePassword(); <--**Don't think this was needed?
   // password gen. window prompts
-  var passwordLength = window.prompt('Submit your password length, between 8 and 128 characters.');
+  var passwordLength = window.prompt('Submit your desired password length');
+  
   var lowercaseConfirm = window.confirm('Do you want lowercase letters?');
   if (lowercaseConfirm) {
       window.alert("You added lowercase letters.");
     rawpassword += characters.lowercase;
+  }
+  else {
+    window.alert("You did not select lowercase letters.")
   };
+  
   var uppercaseConfirm = window.confirm('Do you want uppercase letters?');
   if (uppercaseConfirm) {
       window.alert("You added uppercase letters.");
     rawpassword += characters.uppercase;
   }
+  else {
+    window.alert("You did not select uppercase letters.")
+  };
+
   var symbolConfirm = window.confirm('Do you want symbols?');
   if (symbolConfirm) {
       window.alert("You added symbols.");
     rawpassword += characters.symbol;
   }
+  else {
+    window.alert("You did not select symbols.")
+  };
+
   var numberConfirm = window.confirm('Do you want numbers?');
   if (numberConfirm) {
     window.alert("You added numbers.");
     rawpassword += characters.number;
   }
+  else {
+    window.alert("You did not select numbers.")
+  };
+
+
 // Password gen. reset if no selections made!
   if (!lowercaseConfirm && !uppercaseConfirm && !symbolConfirm && !numberConfirm) {
     window.alert("You must make at least one character type selection. Please try again!");
